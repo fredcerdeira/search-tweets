475fefbfa8c4eaa00891d08ee058a51349c7bb2c
fredcerdeira <frederico.cerdeira@gmail.com>
2013-04-07 04:04:53 +0100

    updated all

--- js/main.js
+++ js/main.js
@@ -16,34 +16,66 @@ $(document).ready(function() {
 		} else {
 			$('.status-msg').removeClass('hidden');
 		}
-
-
 	
 	});
+
+	showAbout();
 
 
 });
 
 
+function showAbout(){
+	$('#show-content').click(function(e) {
+		e.preventDefault();
+
+		var elToShow = $('.about');
+
+
+		if (elToShow.hasClass('hidden')){
+			elToShow.removeClass('hidden');
+			$(this).addClass('active');
+		} else {
+			elToShow.addClass('hidden');
+			$(this).removeClass('active');
+		} 
+		
+	})
+}
+
 
 function search(searchTerm) {
+	
 	$('.status-msg').addClass('hidden');
 	$('.loading').show();
 	
+
 	$.ajax({
 		url: 'https://search.twitter.com/search.json?' + $.param(searchTerm) + '&rpp=5',
 		dataType: 'jsonp',
 		success: function(data) {
+			
+			//console.dir (data);
+			$('.loading').hide();
+
+			if (data['results'].length == 0){ $('.status-msg').removeClass('hidden').html('No Tweets with that term!');};
 
 			$('.wrapper').append('<ul class="results js-tweets"></ul>');
 
 			for (item in data['results']) {
+
+				var userPic  	= data['results'][item]['profile_image_url'].replace('_normal', '_bigger'),
+					userText 	= data['results'][item]['text'],
+					userId		= data['results'][item]['from_user'],
+					tweetUrl 	= 'https://twitter.com/' + userId + '/status/' + data['results'][item]['id_str'];
+
+
 			 	$('.js-tweets').append(
-			    	'<li>' + data['results'][item]['text'] + '</li>'
+			    	'<li><a href="https://twitter.com/'+ userId +'"><img src="'+ userPic +'"></a><a href="'+ tweetUrl +'">'+ userText + '</a></li>'
 			    );
 			}
 
-			$('.loading').hide();
+			
 		}        
 	});
 }
